8eda9243c73c1fec24755e91cb80f87b2a634403
MIXOZ <mihan.oi@mail.ru>
2021-06-02 14:50:08 +0300

    Add getting of url

--- app/src/main/java/com/vk/videodownloader/MainActivity.kt
+++ app/src/main/java/com/vk/videodownloader/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.vk.videodownloader
 
+import android.content.ContentValues.TAG
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
@@ -7,10 +8,15 @@ import android.view.View
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.vk.api.sdk.VK
+import com.vk.api.sdk.VKApiCallback
 import com.vk.api.sdk.VKTokenExpiredHandler
 import com.vk.api.sdk.auth.VKAccessToken
 import com.vk.api.sdk.auth.VKAuthCallback
 import com.vk.api.sdk.auth.VKScope
+import com.vk.sdk.api.friends.FriendsService
+import com.vk.sdk.api.friends.dto.FriendsGetFieldsResponse
+import com.vk.sdk.api.video.VideoService
+import com.vk.sdk.api.video.dto.VideoSaveResult
 
 
 class MainActivity : AppCompatActivity() {
@@ -18,8 +24,45 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        VK.login(this, arrayListOf(VKScope.WALL, VKScope.PHOTOS))
+
+        val text = findViewById<View>(R.id.text) as TextView
+        text.setOnClickListener {
+            VK.execute(VideoService().videoSave(
+                "A",
+                "A",
+                false,
+                false,
+                null,
+                null,
+                null,
+                null,
+                null,
+                false,
+                true,
+                true
+            ), object :
+                VKApiCallback<VideoSaveResult> {
+                override fun success(result: VideoSaveResult) {
+                    text.text =  result.uploadUrl!!
+                    Log.d("URL : ", result.uploadUrl!!)
+                }
+
+                override fun fail(error: Exception) {
+                    Log.e(TAG, error.toString())
+                }
+            })
+        }
+
+        /*
+         * Получаем адрес для загрузки видео
+         */
+        VK.login(this, arrayListOf(VKScope.WALL, VKScope.PHOTOS, VKScope.VIDEO))
         //VK.addTokenExpiredHandler(tokenTracker)
+
+
+
+
+
     }
 
 //    private val tokenTracker = object: VKTokenExpiredHandler {
@@ -29,7 +72,7 @@ class MainActivity : AppCompatActivity() {
 //    }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        val callback = object: VKAuthCallback {
+        val callback = object : VKAuthCallback {
             override fun onLogin(token: VKAccessToken) {
                 val text = findViewById<View>(R.id.text) as TextView
                 text.text = ("successful").toString()
